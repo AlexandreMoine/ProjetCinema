f4a16b00b37e7361fb27daabd2815f021eb84cb1
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-12-23 14:52:37 +0100

    DEV : add ImagesController

--- src/main/java/com/epul/cinema/CinemaApplication.java
+++ src/main/java/com/epul/cinema/CinemaApplication.java
@@ -3,9 +3,13 @@ package com.epul.cinema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class CinemaApplication extends SpringBootServletInitializer {
 
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
